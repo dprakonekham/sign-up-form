551b855cf10e0632d6f253c9c268eb8715ec6500
Danny <dprakonekham@gmail.com>
2023-03-04 02:57:15 -0800

    added check to see if passwords match

---
+++ script.js
@@ -0,0 +1,21 @@
+//Checking if the passwords match
+function checkPassword(){
+    const form = document.querySelector(`form[name="signupForm"]`);
+    const passwordInput = form.elements[`password`].value;
+    const passwordInputConfirm = form.elements[`confirm_password`].value;
+    const confirmPassword = document.getElementById('confirm_password');
+
+    console.log(passwordInput)
+    console.log(passwordInputConfirm)
+    if(passwordInput != passwordInputConfirm){
+        //If passwords do not match, scroll and focus the confirm password text area
+        confirmPassword.scroll();
+        confirmPassword.focus();
+        confirmPassword.style.borderColor = "red";
+        confirmPassword.setCustomValidity("Passwords do not match");
+    }
+    else{
+        confirmPassword.style.borderColor = "black";
+        confirmPassword.setCustomValidity("");
+    }
+}
